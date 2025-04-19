49cd49fc3c519875dc80f02e0414ddef0e6d641b
Mukher <mukher181@gmail.com>
2025-04-19 15:39:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let weight = prompt("Welcome to BMI Calculator \n Please Enter your weight in kg: ");
+let height = prompt("Please Enter your height in meters: ");
+alert(`Your BMI is: ${weight / (height * height)}`);
